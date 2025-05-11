3fa3633f2ca1abce2c445f208266f94a598d6d19
Ben <benleviae5@gmail.com>
2025-05-11 19:17:49 +0300

    adminDashboard2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@
 import React from "react";
 import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>DeliZariz User System</h1>
-      <RegisterForm />
-      <hr />
-      <LoginForm />
-    </div>
+    <BrowserRouter>
+      <div style={{ padding: "20px" }}>
+        <h1>DeliZariz User System</h1>
+        <RegisterForm />
+        <hr />
+        <LoginForm />
+      </div>
+      <Routes>
+        <Route path="/admindash" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
